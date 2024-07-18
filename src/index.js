3fb75dec7196bf0a7bbc4d9e6429fa4d8432cc0e
Joseph Mitchell <joejmitchell@hotmail.co.uk>
2024-07-18 19:30:33 +0100

    Added LanguageModelRouter variable to index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import Config from './config/Config.js';
 import Server from "./server/Server.js";
-import Database from "./database/database.js";
+import Database from "./database/Database.js";
+import LanguageModelRouter from './routers/LanguageModel.router.js';
+import LanguageModelController from './controllers/LanguageModel.controller.js';
+import LanguageModelService from './services/LanguageModel.service.js';
 
 Config.load();
 const { PORT, HOST, DB_URI } = process.env;
 
-const routers = [];
+const languageModelRouter = new LanguageModelRouter(new LanguageModelController(new LanguageModelService()), "/models")
+const routers = [languageModelRouter];
 const server = new Server(PORT, HOST, routers);
 const database = new Database(DB_URI);
 
