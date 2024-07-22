f033598663f1f75a829a585c19e675b6a06e3617
Joseph Mitchell <joejmitchell@hotmail.co.uk>
2024-07-22 19:38:11 +0100

    FIX: added imports for account classes in index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import Database from "./database/Database.js";
 import LanguageModelRouter from './routers/LanguageModel.router.js';
 import LanguageModelController from './controllers/LanguageModel.controller.js';
 import LanguageModelService from './services/LanguageModel.service.js';
+import AccountRouter from './routers/Account.router.js';
+import AccountController from './controllers/Account.controller.js';
+import AccountService from './services/Account.service.js';
 
 Config.load();
 const { PORT, HOST, DB_URI } = process.env;
