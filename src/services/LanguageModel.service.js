ab4ef2dfd413778c5b40c3cedd4ad8daa19bb13b
Joseph Mitchell <joejmitchell@hotmail.co.uk>
2024-07-24 14:38:42 +0100

    LanguageModelController.editModel: Unit Test 1-3 Complete

--- src/services/LanguageModel.service.js
+++ src/services/LanguageModel.service.js
@@ -34,4 +34,26 @@ export default class LanguageModelService {
             feedback: feedback,
         });
     }
+    
+    async editModel(id, name, organization, description, date_created, url, datasheet_url, modality, model_analysis, size, dependencies, quality_control, access, license, intended_uses, prohibited_uses, monitoring, feedback) {
+        return await LanguageModel.findByIdAndUpdate(id, {
+            name: name,
+            organization: organization,
+            description: description,
+            date_created: date_created,
+            url: url,
+            datasheet_url: datasheet_url,
+            modality: modality,
+            model_analysis: model_analysis,
+            size: size,
+            dependencies: dependencies,
+            quality_control: quality_control,
+            access: access,
+            license: license,
+            intended_uses: intended_uses,
+            prohibited_uses: prohibited_uses,
+            monitoring: monitoring,
+            feedback: feedback,
+        });
+    }
 }
--- test/controllers/LanguageModel.controller.test.js
+++ test/controllers/LanguageModel.controller.test.js
@@ -287,4 +287,108 @@ describe("Language Model Controller", () => {
             sinon.assert.calledWith(stubbedResponse.status, 400);
         });
     });
+    
+    describe("createModel", () => {
+        let responseModel;
+        
+        beforeEach(() => {
+            responseModel = {};
+            
+            stubbedModelService = {
+                editModel: sinon.stub(),
+            };
+            stubbedAccountService = {
+                getAccountRoleById: sinon.stub(),
+            };
+            stubbedResponse = {
+                status: sinon.stub().returnsThis(),
+                json: sinon.stub()
+            };
+            
+            testController = new LanguageModelController(stubbedModelService, stubbedAccountService);
+            testRequest = {
+                params: { id: 1234 }, body: {
+                    name: newModels.normalModel.name,
+                    organization: newModels.normalModel.organization,
+                    description: newModels.normalModel.description,
+                    date_created: newModels.normalModel.date_created,
+                    url: newModels.normalModel.url,
+                    datasheet_url: newModels.normalModel.datasheet_url,
+                    modality: newModels.normalModel.modality,
+                    model_analysis: newModels.normalModel.model_analysis,
+                    size: newModels.normalModel.size,
+                    dependencies: newModels.normalModel.dependencies,
+                    quality_control: newModels.normalModel.quality_control,
+                    access: newModels.normalModel.access,
+                    license: newModels.normalModel.license,
+                    intended_uses: newModels.normalModel.intended_uses,
+                    prohibited_uses: newModels.normalModel.prohibited_uses,
+                    monitoring: newModels.normalModel.monitoring,
+                    feedback: newModels.normalModel.feedback,
+                }
+            };
+            
+            stubbedModelService.editModel.resolves({ models: responseModel });
+            stubbedAccountService.getAccountRoleById.resolves({ role: { admin_permissions: true } });
+        });
+        
+        afterEach(() => {
+            responseModel = undefined;
+            stubbedModelService = undefined;
+            stubbedAccountService = undefined;
+            stubbedResponse = undefined;
+            testController = undefined;
+        });
+        
+        it("should respond 204 in normal circumstances", async () => {
+            //Act
+            await testController.editModel(testRequest, stubbedResponse);
+
+            //Assert
+            sinon.assert.calledWith(stubbedAccountService.getAccountRoleById, testRequest.body.id);
+            sinon.assert.calledWith(stubbedModelService.editModel,
+                testRequest.params.id,
+                testRequest.body.name,
+                testRequest.body.organization,
+                testRequest.body.description,
+                testRequest.body.date_created,
+                testRequest.body.url,
+                testRequest.body.datasheet_url,
+                testRequest.body.modality,
+                testRequest.body.model_analysis,
+                testRequest.body.size,
+                testRequest.body.dependencies,
+                testRequest.body.quality_control,
+                testRequest.body.access,
+                testRequest.body.license,
+                testRequest.body.intended_uses,
+                testRequest.body.prohibited_uses,
+                testRequest.body.monitoring,
+                testRequest.body.feedback,
+            );
+            sinon.assert.calledWith(stubbedResponse.status, 204);
+        });
+        
+        it("should respond 403 if account service call resolves with undefined admin_permissions", async () => {
+            //Arrange
+            stubbedAccountService.getAccountRoleById.resolves({ role: {} });
+            
+            //Act
+            await testController.editModel(testRequest, stubbedResponse);
+
+            //Assert
+            sinon.assert.calledWith(stubbedResponse.status, 403);
+        });
+        
+        it("should respond 400 if model service call resolves null", async () => {
+            //Arrange
+            stubbedModelService.editModel.resolves(null);
+            
+            //Act
+            await testController.editModel(testRequest, stubbedResponse);
+
+            //Assert
+            sinon.assert.calledWith(stubbedResponse.status, 400);
+        });
+    });
 });
